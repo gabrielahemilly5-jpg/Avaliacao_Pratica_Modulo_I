5cf87690789f4ef165586cb795b7fbf79c3e531c
gabrielahemilly5-jpg <gabrielahemilly5@gmail.com>
2026-08-08 14:52:44 -0300

    prova modulo 1
    Hemilly

---
+++ Prova_imc.py
@@ -0,0 +1,21 @@
+nome = input("Digite seu nome: ")
+peso = float(input("Digite seu peso (kg): "))
+altura = float(input("Digite sua altura (m): "))
+
+imc = peso / (altura ** 2)
+
+print("Nome:", nome)
+print(f"IMC: {imc:.2f}")
+
+if imc <=18.5:
+    print("Abaixo do peso")
+elif imc <= 24.9:
+    print("Peso normal")
+elif imc <=29.9:
+    print("Sobrepeso")
+elif imc <=34.9:
+    print("Obesidade Grau I")
+elif imc <=39.9:
+    print("Obesidade Grau II")
+else:
+    print("Obesidade Grau III (mórbida)")
